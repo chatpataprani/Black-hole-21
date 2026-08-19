561dda92a82babfb27dab64ba3ff092b5aac5156
chatpataprani <garpthefist11@gmail.com>
2026-08-19 17:22:31 +0530

    feat: add web FCM service worker to Capacitor assets

---
+++ www/firebase-messaging-sw.js
@@ -0,0 +1,23 @@
+/* Black Hole 21 web FCM service worker. Public Firebase web config only; no private credentials. */
+self.addEventListener("notificationclick", (event) => {
+  event.notification.close();
+  event.waitUntil(
+    clients.matchAll({ type: "window", includeUncontrolled: true }).then((clientList) => {
+      for (const client of clientList) if ("focus" in client) return client.focus();
+      if (clients.openWindow) return clients.openWindow("https://black-hole-21.onrender.com/");
+      return undefined;
+    })
+  );
+});
+importScripts("https://www.gstatic.com/firebasejs/12.1.0/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/12.1.0/firebase-messaging-compat.js");
+firebase.initializeApp({
+  apiKey: "AIzaSyBkqB_t3LeWON0XZTQnrbStZsXKxkzVAhg",
+  authDomain: "black-hole-21.firebaseapp.com",
+  projectId: "black-hole-21",
+  storageBucket: "black-hole-21.firebasestorage.app",
+  messagingSenderId: "8258393999",
+  appId: "1:8258393999:web:291ffec222a173f7d71dfa",
+  measurementId: "G-E8NGTPHHEZ"
+});
+firebase.messaging();
